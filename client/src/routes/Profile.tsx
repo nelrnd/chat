5ee444eed0f2c5aa61b6d01d08207722801648ac
Nel <nel.renaudin@gmail.com>
2024-05-02 19:42:17 +0200

    create Profile page

--- client/src/routes/Profile.tsx
+++ client/src/routes/Profile.tsx
@@ -1,3 +1,14 @@
+import { useAuth } from "../providers/AuthProvider"
+
 export default function Profile() {
-  return <h1>Profile</h1>
+  const { authUser } = useAuth()
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>
+        {authUser?.name} | {authUser?.email}
+      </p>
+    </div>
+  )
 }
