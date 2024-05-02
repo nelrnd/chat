9df2584044b664f35bc4ca2bfd061b868dc6cb7f
Nel <nel.renaudin@gmail.com>
2024-05-02 13:30:44 +0200

    implement socket.io on server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require("express")
-
 const app = express()
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+io.on("connection", (socket) => {
+  console.log("a user connected")
+})
+
 app.get("/", (req, res) => res.send("Hello"))
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
