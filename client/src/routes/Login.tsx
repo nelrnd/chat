9fe280ffc41774d3f83294d4513cd269ede8d3bd
Nel <nel.renaudin@gmail.com>
2024-05-02 15:10:23 +0200

    create forms with react hook form in Register and Login

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,9 +1,36 @@
+import { SubmitHandler, useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 
+type Inputs = {
+  email: string
+  password: string
+}
+
 export default function Login() {
+  const { register, handleSubmit } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div>
       <h1>Login</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" {...register("email")} required />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" {...register("password")} required />
+        </div>
+
+        <button>Login</button>
+      </form>
+
       <p>
         Don't have an account yet? <Link to="/register">Register</Link>
       </p>
--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -1,9 +1,42 @@
+import { SubmitHandler, useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 
+type Inputs = {
+  name: string
+  email: string
+  password: string
+}
+
 export default function Register() {
+  const { register, handleSubmit } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
+
   return (
     <div>
       <h1>Register</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="name">Display name</label>
+          <input id="name" {...register("name")} required />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" {...register("email")} required />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" {...register("password")} required />
+        </div>
+
+        <button>Register</button>
+      </form>
+
       <p>
         Have an account already? <Link to="/login">Login</Link>
       </p>
