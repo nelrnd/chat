9af9286606337a34b722b32daf10c7f61feb8c8b
Nel <nel.renaudin@gmail.com>
2024-05-02 20:38:28 +0200

    display formatted date next to message

--- client/src/routes/Chat.tsx
+++ client/src/routes/Chat.tsx
@@ -1,3 +1,35 @@
+import axios from "axios"
+import moment from "moment"
+import { useEffect, useState } from "react"
+
+type Message = {
+  _id: string
+  content: string
+  sender: { _id: string; name: string; email: string }
+  timestamp: number
+}
+
 export default function Chat() {
-  return <h1>Chat</h1>
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    axios.get("/message").then((res) => setMessages(res.data))
+  }, [])
+
+  return (
+    <div>
+      <h1>Chat</h1>
+
+      {messages.length && (
+        <ul>
+          {messages.map((msg) => (
+            <li key={msg._id}>
+              <strong>{msg.sender.name}: </strong>
+              {msg.content} - {moment(msg.timestamp).format("LT")}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
