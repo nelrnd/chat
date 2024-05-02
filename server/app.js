d8dc070a2908744427f9e9b7395066ef8ab62497
Nel <nel.renaudin@gmail.com>
2024-05-02 13:35:52 +0200

    connect to database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const http = require("http")
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
+const mongoose = require("mongoose")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// connect to db
+const mongoDb = process.env.MONGODB_URL
+const main = async () => mongoose.connect(mongoDb)
+main().catch((err) => console.log(err))
 
 io.on("connection", (socket) => {
   console.log("a user connected")
