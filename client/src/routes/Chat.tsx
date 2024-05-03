1e292049e92d31f8dbe4430152e8ebd4e8323030
Nel <nel.renaudin@gmail.com>
2024-05-03 11:18:32 +0200

    add new message to message list and redirect in Home if logged in

--- client/src/routes/Chat.tsx
+++ client/src/routes/Chat.tsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import moment from "moment"
 import { useEffect, useState } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
 
 type Message = {
   _id: string
@@ -9,12 +10,30 @@ type Message = {
   timestamp: number
 }
 
+interface Inputs {
+  content: string
+}
+
 export default function Chat() {
+  const { register, handleSubmit, reset } = useForm<Inputs>()
   const [messages, setMessages] = useState<Message[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     axios.get("/message").then((res) => setMessages(res.data))
   }, [])
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    setLoading(true)
+    try {
+      const res = await axios.post("/message", data)
+      setMessages((prev) => [...prev, res.data])
+      reset()
+    } catch (err) {
+      console.log(err)
+    }
+    setLoading(false)
+  }
 
   return (
     <div>
@@ -30,6 +49,11 @@ export default function Chat() {
           ))}
         </ul>
       )}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <textarea placeholder="Type something" {...register("content")} required spellCheck="false"></textarea>
+        <button disabled={loading}>{loading ? "Loading..." : "Submit"}</button>
+      </form>
     </div>
   )
 }
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../providers/AuthProvider"
+import { useEffect } from "react"
 
 export default function Home() {
+  const { token } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token) {
+      navigate("/chat")
+    }
+  }, [token, navigate])
+
   return (
     <div>
       <h1>Home</h1>
