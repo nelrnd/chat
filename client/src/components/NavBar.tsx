f48883a9955ea3b3413890882ed1e2ef1ad2d85e
Nel <nel.renaudin@gmail.com>
2024-06-26 15:19:07 +0200

    make NavBar and Footer responsive

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,11 +4,11 @@ import logo from "../assets/logo.svg"
 export default function NavBar() {
   return (
     <nav>
-      <div className="max-w-[56rem] h-[6rem] m-auto flex justify-between items-center border-b border-neutral-800">
+      <div className="max-w-[56rem] h-[6rem] m-auto p-3 flex justify-between items-center border-b border-neutral-800">
         <Link to="/" className="p-2 -ml-2">
           <img src={logo} alt="MiChat" />
         </Link>
-        <ul className="flex gap-6">
+        <ul className="flex gap-3 md:gap-6">
           <li>
             <Link to="/about" className="font-medium text-neutral-400 hover:text-white transition-colors p-2">
               About
