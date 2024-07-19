f06b2007e566d3e08f6a1b28b809d2b70d12ffa4
Nel <nel.renaudin@gmail.com>
2024-07-19 17:32:13 +0200

    update socket.ts

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
 
-const URL = import.meta.env.MODE === "production" ? undefined : "http://localhost:3000"
+const URL = import.meta.env.MODE === "production" ? import.meta.env.VITE_SERVER_BASE_URL : "http://localhost:3000"
 
 export const socket = io(URL, { autoConnect: false })
